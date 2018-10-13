ef914831109e936fec72f36afd6789b20508fe34
vagrant <vagrant@vagrant.vm>
2018-10-13 06:58:38 +0000

    added homepage route in server file.

--- server.py
+++ server.py
@@ -11,3 +11,23 @@ from flask_debugtoolbar import DebugToolbarExtension
 from werkzeug import secure_filename
 import datetime
 from model import User, Event, Picture, connect_to_db, db
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'EVENTFULL'
+
+
+################
+#### routes ####
+################
+
+# NEED TO BE LOGGED IN BEFORE BEING ABLE TO DO ANYTHING?
+
+@app.route('/')
+def homepage():
+    """Show homepage."""
+    # if 'user_id' in session:
+    #     return redirect('/user')
+
+    # else:
+    return render_template('homepage.html')
